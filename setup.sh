3f8958bf6c3e6eb5327eb6de85d174c15a811f99
Jean-Michel Gorius <jean-michel.gorius@irisa.fr>
2025-06-20 16:12:40 +0200

    Add parameters to enable/disable debug info

--- setup.sh
+++ setup.sh
@@ -3,15 +3,23 @@
 set -euo pipefail
 
 install=0
+cmake_build_type="Debug"
+llvm_cmake_build_type="Release"
 
-while getopts "h?i" opt; do
+while getopts "h?idD" opt; do
   case "$opt" in
   h | \?)
-    echo -e "Usage: $0 [-i]\n\nOptions:\n    -i    Install to prefix"
+    echo -e "Usage: $0 [-i]\n\nOptions:\n    -i    Install to prefix\n    -r    Build spechls-circt in release mode\n    -D    Build LLVM with debug information"
     exit 0
     ;;
   i)
     install=1
+    ;;
+  d)
+    cmake_build_type="Release"
+    ;;
+  D)
+    llvm_cmake_build_type="RelWithDebInfo"
     ;;
   esac
 done
@@ -29,7 +37,7 @@ make -C yosys install PREFIX="$PREFIX"
 
 # Build LLVM
 mkdir -p circt/llvm/build && cd circt/llvm/build
-cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_PARALLEL_LINK_JOBS=2 -DCMAKE_INSTALL_PREFIX="$PREFIX" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_OCAMLDOC=OFF -DLLVM_ENABLE_BINDINGS=OFF
+cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DCMAKE_BUILD_TYPE="$llvm_cmake_build_type" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD="host" -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_PARALLEL_LINK_JOBS=2 -DCMAKE_INSTALL_PREFIX="$PREFIX" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_OCAMLDOC=OFF -DLLVM_ENABLE_BINDINGS=OFF
 cmake --build .
 if [ "$install" -eq 1 ]; then
   cmake --build . --target install
@@ -38,7 +46,7 @@ cd "$ROOT_DIR"
 
 # Build CIRCT
 mkdir -p circt/build && cd circt/build
-cmake -G Ninja .. -DMLIR_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/mlir" -DLLVM_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/llvm" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="$PREFIX"
+cmake -G Ninja .. -DMLIR_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/mlir" -DLLVM_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/llvm" -DCMAKE_BUILD_TYPE="$llvm_cmake_build_type" -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="$PREFIX"
 cmake --build .
 if [ "$install" -eq 1 ]; then
   cmake --build . --target install
@@ -47,7 +55,7 @@ cd "$ROOT_DIR"
 
 # Build spechls-circt
 mkdir -p spechls-circt/build && cd spechls-circt/build
-cmake -G Ninja .. -DYosys_LIBRARY="$ROOT_DIR/yosys/libyosys.so" -DYosys_INCLUDE_DIR="$ROOT_DIR/yosys" -DMLIR_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/mlir" -DCIRCT_DIR="$ROOT_DIR/circt/build/lib/cmake/circt" -DLLVM_EXTERNAL_LIT="$ROOT_DIR/circt/llvm/build/bin/llvm-lit" -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="$PREFIX" -DUSE_ALTERNATE_LINKER=mold -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -G Ninja .. -DCMAKE_BUILD_TYPE="$cmake_build_type" -DYosys_LIBRARY="$ROOT_DIR/yosys/libyosys.so" -DYosys_INCLUDE_DIR="$ROOT_DIR/yosys" -DMLIR_DIR="$ROOT_DIR/circt/llvm/build/lib/cmake/mlir" -DCIRCT_DIR="$ROOT_DIR/circt/build/lib/cmake/circt" -DLLVM_EXTERNAL_LIT="$ROOT_DIR/circt/llvm/build/bin/llvm-lit" -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="$PREFIX" -DUSE_ALTERNATE_LINKER=mold -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build .
 if [ "$install" -eq 1 ]; then
   cmake --build . --target install
